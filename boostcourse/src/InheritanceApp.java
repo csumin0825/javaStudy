3be068fc125524581b7408f9c1167a2446dacfa1
sumin <csumin008255@gmail.com>
2023-03-20 10:53:04 +0900

    23.03.20_Overriding,Overloading

--- boostcourse/src/InheritanceApp.java
+++ boostcourse/src/InheritanceApp.java
@@ -1,21 +1,33 @@
-// Cal을 수정할 수 없음.
 class Cal{
 	public int sum(int v1, int v2) {
 		return v1+v2;
 	}
+	// Overloading 오버로
+	public int sum(int v1, int v2, int v3) {
+		return this.sum(v1, v2)+v3;
+	}
 }
 
 class Cal3 extends Cal{
-	
+	public int minus(int v1, int v2) {
+		return v1-v2;
+	}
+	// Overriding 오버라이딩
+	public int sum(int v1, int v2) {
+		System.out.println("Cal3!");
+		return super.sum(v1, v2);
+	}
 }
 
 public class InheritanceApp{
-
 	public static void main(String[] args) {
 		Cal c = new Cal();
 		System.out.println(c.sum(2, 1));
+		System.out.println(c.sum(2, 1, 5));
 		
 		Cal3 c3 = new Cal3();
+		System.out.println(c3.sum(2, 2));
+		System.out.println(c3.minus(2, 1));
 		System.out.println(c3.sum(2, 2));
 	}
 
