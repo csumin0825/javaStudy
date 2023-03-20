84f052a1ed294bc19960cb42a076edfb6e284914
sumin <csumin008255@gmail.com>
2023-03-20 11:15:37 +0900

    inheritance

--- boostcourse/src/InheritanceApp.java
+++ boostcourse/src/InheritanceApp.java
@@ -1,34 +1,28 @@
 class Cal{
-	public int sum(int v1, int v2) {
-		return v1+v2;
+	int v1,v2;
+	Cal(int v1, int v2){
+		System.out.println("Cal init!");
+		this.v1 = v1; this.v2 = v2;
 	}
-	// Overloading 오버로
-	public int sum(int v1, int v2, int v3) {
-		return this.sum(v1, v2)+v3;
-	}
+	public int sum() {return this.v1+this.v2;}
 }
-
+// 부모가 생성자가 있으면 자식클래스도 부모 생성자를 따라야함. 
 class Cal3 extends Cal{
-	public int minus(int v1, int v2) {
-		return v1-v2;
+
+	Cal3(int v1, int v2) {
+		super(v1, v2); // 부모클래스의 생성
+		System.out.println("Cal3 init!");
 	}
-	// Overriding 오버라이딩
-	public int sum(int v1, int v2) {
-		System.out.println("Cal3!");
-		return super.sum(v1, v2);
-	}
-}
+	public int minus() {return this.v1-this.v2;}
+} 
 
 public class InheritanceApp{
 	public static void main(String[] args) {
-		Cal c = new Cal();
-		System.out.println(c.sum(2, 1));
-		System.out.println(c.sum(2, 1, 5));
+		Cal c = new Cal(2,1);
 		
-		Cal3 c3 = new Cal3();
-		System.out.println(c3.sum(2, 2));
-		System.out.println(c3.minus(2, 1));
-		System.out.println(c3.sum(2, 2));
+		Cal3 c3 = new Cal3(3,1);
+		System.out.println(c3.sum());
+		System.out.println(c3.minus());
 	}
 
 }
